2f864182947cea3f541ccfe0d8ba182b1ecab739
Shashank Mandil <mandil.shashank@gmail.com>
2016-12-10 02:16:40 -0800

    Adding try catch block for main data pulling for every symbol

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,9 +6,13 @@ object Main {
   def main(args: Array[String]): Unit = {
     val bufferedSource = io.Source.fromFile("constituents.csv")
     for (line <- bufferedSource.getLines.drop(1)) {
-      val Array(symbol,name,sector) = line.split(",").map(_.trim)
-      val stk = new Stock(symbol)
-      stk.getHistoricalData((new DateTime).withDate(2015,1,1), (new DateTime).withDate(2016,11,30))
+      try {
+        val Array(symbol, name, sector) = line.split(",").map(_.trim)
+        val stk = new Stock(symbol)
+        stk.getHistoricalData((new DateTime).withDate(2015, 1, 1), (new DateTime).withDate(2016, 11, 30))
+      } catch {
+        case e => e.printStackTrace
+      }
     }
     bufferedSource.close
   }
